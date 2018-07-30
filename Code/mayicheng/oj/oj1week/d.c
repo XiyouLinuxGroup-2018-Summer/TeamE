f04b605c7054e64884d8595736f643dad9f93365
ghroges <1298394633@qq.com>
2018-07-30 09:12:02 +0800

    test

--- Code/mayicheng/oj/oj1week/d.c
+++ Code/mayicheng/oj/oj1week/d.c
@@ -5,11 +5,37 @@ int main()
 	int a,b;
 	while(scanf("%d%d",&a,&b) != EOF)
 	{
-		int s;
-		while(1)
+		int aaa = a * b;
+		if(a == 0 || b == 0)
 		{
-			if()
+			printf("0\n");
+			continue;
 		}
+		int mm = a;
+		while(a != b)
+		{
+			if(a < b)
+			{
+				int t = a;
+				a = b;
+				b = t;
+			}
+			while(a >= b)
+			{
+				a -= b;
+			}
+			if(a == 0)
+			{
+				mm = b;
+				break;
+			}
+		}
+		if(mm == 0)
+		{
+			printf("0\n");
+			continue;
+		}
+		printf("%d\n",aaa / mm);
 	}
 	return 0;
 }
