67f7e9aaf040735de1f941cd7f947e767524c569
妖冶如你 <yaoyeruni@yaoyerunideAir.lan>
2018-08-06 19:21:36 +0800

    x

---
+++ Code/zhuyichen/apue/cond.c
@@ -0,0 +1,16 @@
+#include <pthread.h>
+
+struct msg {
+    struct msg *m_next;
+};
+
+struct msg *workq;
+
+pthread_cond_t qready = PTHREAD_COND_INITIALIZER;
+
+pthread_mutex_t qlock = PTHREAD_MUTEX_INITIALIZER;
+
+void process_msg(void)
+{
+
+}
