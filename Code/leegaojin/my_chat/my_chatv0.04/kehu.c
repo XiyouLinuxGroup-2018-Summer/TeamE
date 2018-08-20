6dbf1c91f362ff7613bcc8ba72f08c7e1d6e9ef5
lgjh123 <lgjh123@163.com>
2018-08-20 21:51:42 +0800

    week5day22

--- Code/leegaojin/my_chat/my_chatv0.04/kehu.c
+++ Code/leegaojin/my_chat/my_chatv0.04/kehu.c
@@ -7,50 +7,32 @@
 #include<errno.h>
 #include<netinet/in.h>
 #include<arpa/inet.h>
-#include<pthread.h>
+#include<pthread.h> 
 #include<string.h>
 
 #define CHAT_O     4//私聊功能
 #define REGISTER   1 // 注册
 #define LOGIN      2 //  登录
 #define QUIT       3 //退出  //用户下线
+#define ADD_FRI    5 //添加好友
 
-#define REG_SUC    10001 //注册成功
-#define LOG_SUC    10002 //登陆成功
-#define LOG_FAU    10003 //登录失败
+#define REG_SUC     10001 //注册成功
+#define LOG_SUC     10002 //登陆成功
+#define LOG_FAU     10003 //登录失败
+#define ADD_FRI_SCU 10004//添加好友成功
+#define ADD_FRI_FAU 10005//添加好友失败
+#define ACC_ADD     10006//好友请求
 
 #define PORT 8848
 /*请求-数据包  包含 操作标记 发送方 接收方  信息
 */
 typedef struct masg{
   int flag;
-  char recv_mum[20];
+  char recv_mem[20];
   char send_mem[20];
   char data[1024];
 }MASG;
 /***********请求-数据包**************/
-/***************个人界面******************/
-void meun_per(void)
-{
-  puts(">我的好友");
-  puts(">我的群");
-  puts(" ");
-  puts("1.添加好友 2.删除好友")
-
-}
-/****************************************/
-/*****************登录注册界面**************/
-void meun_rll(void)
-{
-  puts("          welcome  to  my_chat              ");
-  puts("--------------------------------------------");
-  puts("|         1.register 2.login               |");
-  puts("|         3.exit                           |");
-  puts("--------------------------------------------");
-  puts("                            my_chat v0.03  ");
-}
-/********************************/
-
 /*退出函数**********/
 void quit (int socket,int flag)
 {
@@ -59,6 +41,64 @@ void quit (int socket,int flag)
   send(socket,&masg,sizeof(MASG),0);
 }
 /************/
+/***************个人界面******************/
+void meun_per(void)
+{
+  puts(">我的好友");
+  puts(">我的群");
+  puts(" ");
+  puts("1.添加好友 2.删除好友 3.注销登录");
+
+}
+/****************************************/
+/**********加好友******/
+void add_fri (int socket)
+{
+  MASG masg;
+     printf("请输入你想要添加的好友（名字）：");
+     scanf("%s",masg.recv_mem);
+     printf("请输入备注信息（名字）:");
+     scanf("%s",masg.send_mem);
+     masg.flag = ADD_FRI;
+     send(socket,&masg,sizeof(MASG),0);
+}
+
+
+
+/***********个人界面*******************/
+void per(int socket)
+{
+  while(1){
+   meun_per();
+   int flag = 0;
+   scanf("%d",&flag);
+   switch(flag+4)
+   {
+     case ADD_FRI:
+     add_fri(socket);
+     break;
+     case 7 :
+     quit(socket,3);
+     return;
+
+   }
+  }
+}
+/**************************************/
+
+
+/*****************登录注册界面**************/
+void meun_rll(void)
+{
+  puts("          welcome  to  my_chat              ");
+  puts("--------------------------------------------");
+  puts("|         1.register 2.login               |");
+  puts("|         3.exit                           |");
+  puts("--------------------------------------------");
+  puts("                            my_chat v0.04  ");
+}
+/********************************/
+
 /*********
 注册函数
 成功返回1 失败返回-1
@@ -82,6 +122,7 @@ int login(int socket,int flag)
     //发包
     //传回信息，是否成功 //交给主接收函数;
   printf("******************log_end\n");
+  per(socket);    //进入个人界面
 }
 int reg(int socket,int flag)
 {
@@ -106,12 +147,14 @@ int reg(int socket,int flag)
 
 }
 /*********************注册函数*****************/
+
 void my_err(const char* err_string,int line)
 {
   fprintf(stderr,"line:%d ",line);
   perror(err_string);
   exit(1);
 }
+
 void * show_client(void*arg)
 {
   MASG masg;
@@ -136,6 +179,29 @@ void * show_client(void*arg)
       case QUIT :
       printf("我在退出\n");
       pthread_exit(0);
+      case ADD_FRI_SCU:
+      printf("%s 已接受了你的添加请求\n",masg.recv_mem);
+      break;
+      case ADD_FRI_FAU:
+      printf("对方拒绝了你的好友请求\n");
+      break;
+      case ACC_ADD:
+      printf("%s 向你发来一个好友请求\n",masg.send_mem);
+      char acc;
+      printf("是否同意(y/n)\n");
+      scanf("%c",&acc);getchar();
+      if(acc == 'y')
+      {
+        masg.flag = ADD_FRI_SCU;
+        send(socket_c,&masg,sizeof(MASG),0);
+      }
+      else if(acc =='n')
+      {
+        masg.flag = ADD_FRI_FAU;
+        send(socket_c,&masg,sizeof(MASG),0);
+      }
+      break;
+
       default:
       printf("errno!\n");
       break;
---
+++ Code/leegaojin/my_chat/my_chatv0.04/myepoll.c
@@ -0,0 +1,352 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<sys/socket.h>
+#include<unistd.h>
+#include<errno.h>
+#include<netinet/in.h>
+#include<arpa/inet.h>
+#include<fcntl.h>
+#include<sys/epoll.h>
+#include<pthread.h>
+#include<mysql/mysql.h>
+
+#define CHAT_O     4//私聊功能
+#define REGISTER   1 // 注册
+#define LOGIN      2 //  登录
+#define QUIT       3 //退出  //用户下线
+#define ADD_FRI    5 // 添加好友
+
+
+#define REG_SUC     10001 //注册成功
+#define LOG_SUC     10002 //登陆成功
+#define LOG_FAU     10003 //登录失败
+#define ADD_FRI_SCU 10004 //添加好友成功
+#define ADD_FRI_FAU 10005 //添加好友失败
+#define ACC_ADD     10006 //好友请求
+
+#define SERV_PORT     8848 //服务器端口
+#define LISTENQ       12    //链接请求队列的最大长度
+int sock[2]={0};
+/*请求-数据包  包含 操作标记 发送方 接收方  信息
+*/
+typedef struct masg{
+  int flag;
+  char recv_mem[20];
+  char send_mem[20];
+  char data[1024];
+}MASG;
+/************请求-数据包***************/
+/***********添加好友************/
+void add_fri(MYSQL*mysql,MASG*masg,int socket)
+{
+  MYSQL_RES *res = mysql_store_result(mysql);
+  MYSQL_ROW row;
+  char* sql=NULL;
+  sql = (char*)calloc(1024,sizeof(char));
+  sprintf(sql,"SELECT * FROM client_online where name='%s'",masg->recv_mem);
+  if(0 != mysql_real_query(mysql,sql,strlen(sql))){
+    printf("query: %s\n",mysql_error(mysql));
+  }
+  res = mysql_store_result(mysql);
+
+  if ((row = mysql_fetch_row(res))) {
+    
+    printf("%s 发送好友请求成功\n",masg->send_mem);
+    printf("%s %s %s\n",row[0],row[1],row[2]);
+    int socket_send = (int)row[1][0]-'0';
+    masg->flag = ACC_ADD;
+    printf("%d\n",socket_send);
+    send(socket_send,masg,sizeof(MASG),0);
+  }
+  else if ((!mysql_fetch_row(res)))
+  {
+    printf("添加失败！(对方不在线)\n");
+    masg->flag = ADD_FRI_FAU;
+    send(socket,&masg,sizeof(MASG),0);
+  }
+
+}
+
+
+/*清空数据包
+void del_pack (MASG*masg)
+{
+   masg->flag = 0;
+   memset(masg->recv_num,0,sizeof(masg->recv_num))
+   memset(masg->send_mem,0,sizeof(masg->send_mem))
+   memset(masg->data,0,sizeof(masg->data))
+}
+/********************/
+/****退出函数********
+******/
+void setnonblocking(int st) //非阻塞
+{
+    int a = fcntl(st,F_GETFL);
+    if(a < 0)
+    {printf("change fcntl error\n");return;}
+    a = a | O_NONBLOCK;
+    if(fcntl(st,F_SETFL,a) < 0)
+    {
+        printf("set error\n");
+        return;
+    }
+    return;
+}
+
+void my_err(const char* err_string,int line)
+{
+  fprintf(stderr,"line:%d ",line);
+  perror(err_string);
+  exit(1);
+}
+int main()
+{
+      int   sock_fd,conn_fd[5];
+      int   i=0;
+      int   optval;
+      pid_t pid;
+      socklen_t cli_len;
+      struct sockaddr_in  cli_addr,serv_addr;
+      char  recv_buf[128];
+      MASG masg;           //声明结构体
+/*数据库 初始化 链接****/
+      MYSQL mysql;
+    	//初始化MYSQL句柄
+    	if(NULL == mysql_init(&mysql)){
+    		printf("init: %s\n",mysql_error(&mysql));
+    	}
+
+    	//建立MYSQL连接
+
+    	if(NULL == mysql_real_connect(&mysql,"localhost","debian-sys-maint","ZCoY8eh3ichrX4X0","client_system",0,NULL,0)){
+    		printf("connect: %s\n",mysql_error(&mysql));
+    	}
+/******************/
+
+
+      //创建一个tcp套接字
+      sock_fd = socket(AF_INET,SOCK_STREAM,0);
+      //设置该套接字让它可以重新绑定端口
+      optval =1;
+      setsockopt(sock_fd,SOL_SOCKET,SO_REUSEADDR,(void*)&optval,sizeof(int));
+
+      //初始化服务器端地址结构
+      memset(&serv_addr,0,sizeof(struct sockaddr_in));
+      serv_addr.sin_family = AF_INET;
+      serv_addr.sin_port = htons(SERV_PORT);
+      serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+      //将套接字绑定到本地端口
+      if(bind(sock_fd,(struct sockaddr *)&serv_addr,sizeof(struct sockaddr_in))<0)
+      {
+        my_err("bind",__LINE__);
+      }
+
+      //将套接字转化为监听套接字
+    //  if(
+        listen(sock_fd,LISTENQ);
+        //<0);
+    //  {
+    //    my_err("listen",__LINE__);
+    //  }
+      cli_len = sizeof(struct sockaddr_in);
+      printf("epoll初始化........\n");
+      //printf("服务器sock_fd = %d\n",sock_fd);
+      struct epoll_event ev,events[100];
+      int epfd = epoll_create(100);
+      setnonblocking(sock_fd);    //设置非阻塞
+      ev.data.fd = sock_fd;
+      ev.events = EPOLLIN | EPOLLERR | EPOLLHUP;
+      epoll_ctl(epfd,EPOLL_CTL_ADD,sock_fd,&ev);
+
+      int socket_c = 0;
+      int count = 0;
+      while(1)
+      {
+        printf("等待用户操作.......\n");
+        int nfds = epoll_wait(epfd,events,100,-1);
+        printf("操作中........\n");
+        if(nfds == -1)
+        {
+          printf("epoll_wait failed\n");
+          break;
+        }
+        int i;
+        for(i = 0;i<nfds;i++)
+        {
+          //printf("nfds=%d\n",nfds);
+          if(events[i].data.fd<0)
+              continue;
+          if(events[i].data.fd == sock_fd)
+          {
+                socket_c = accept(sock_fd,(struct sockaddr*)&cli_addr,&cli_len);
+                printf("*********用户已链接,ip:%s\n",inet_ntoa(cli_addr.sin_addr));
+                printf("当前用户套接字socket = %d\n",socket_c);
+                if(socket_c > 0)
+                {
+                  setnonblocking(sock_fd);
+                  ev.data.fd = socket_c;
+                  ev.events = EPOLLIN | EPOLLERR | EPOLLHUP;
+                  epoll_ctl(epfd,EPOLL_CTL_ADD,socket_c,&ev);
+                  continue;
+                }
+          }
+
+          if(events[i].events & EPOLLERR) //?????
+          {
+            socket_c = events[i].data.fd;
+            close(socket_c);
+            epoll_ctl(epfd,EPOLL_CTL_DEL,socket_c,NULL);
+            continue;
+          }
+          if(events[i].events & EPOLLHUP)  //????
+          {
+              socket_c = events[i].data.fd;
+              close(socket_c);
+              epoll_ctl(epfd,EPOLL_CTL_DEL,socket_c,NULL);
+              continue;
+          }
+
+          if(events[i].events & EPOLLIN)    //遇到事件！！！
+          {
+          socket_c = events[i].data.fd;
+          if(socket_c<=0)
+          {
+            close(socket_c);
+            epoll_ctl(epfd,EPOLL_CTL_DEL,socket_c,NULL);
+            continue;
+          }
+          //char  recv_buf[1024];
+          if(recv(socket_c,&masg,sizeof(MASG),0)<0)
+          {
+            perror("recv");
+            exit(1);
+          }
+
+          printf("当前操作socket_c = %d\n",socket_c);
+          //解包   查找选项
+
+          int flag = masg.flag; //获取消息类型
+          printf("获取消息类型为:%d\n",flag);
+          
+
+            switch (flag) 
+            {
+              case CHAT_O :
+              printf("........私聊操作...");
+                if(socket_c==sock[0])
+                send(sock[1],masg.data,sizeof(masg.data),0);
+                if(socket_c==sock[1])
+                send(sock[0],masg.data,sizeof(masg.data),0);
+                printf("客户端发送消息：%s\n",masg.data);
+
+                memset(&masg,0,sizeof(MASG));
+                printf("用户操作结束.........[返回等待]\n\n");
+                break;
+
+
+              case REGISTER :
+
+              printf(".........注册.........\n");
+              //执行插入语句
+              char* sql=NULL;
+              sql = (char*)calloc(1024,sizeof(char));
+              sprintf(sql,"INSERT INTO client_member(name,passwd) VALUES('%s','%s')",masg.send_mem,masg.data);
+              if(0 != mysql_real_query(&mysql,sql,strlen(sql))){
+                printf("query: %s\n",mysql_error(&mysql));
+              }
+              
+              masg.flag = REG_SUC;
+              send(socket_c,&masg,sizeof(MASG),0);
+              //执行查询语句
+              sql = "SELECT id,name,passwd FROM client_member";
+              if(0 != mysql_real_query(&mysql,sql,strlen(sql))){
+                printf("query: %s\n",mysql_error(&mysql));
+              }
+              MYSQL_RES *res = mysql_store_result(&mysql);
+              MYSQL_ROW row;
+              while ((row = mysql_fetch_row(res))) {
+                printf("%s %s %s\n",row[0],row[1],row[2]);
+              }
+              //memset(sql,0,sizeof(sql));
+              //mysql_free_result(res);
+              //mysql_close(&mysql);
+                break;
+
+              case LOGIN:
+
+              sprintf(sql,"SELECT * FROM client_member where name='%s' and passwd='%s'",masg.send_mem,masg.data);
+              if(0 != mysql_real_query(&mysql,sql,strlen(sql))){
+                printf("query: %s\n",mysql_error(&mysql));
+              }
+              res = mysql_store_result(&mysql);
+
+              if ((row = mysql_fetch_row(res))) {
+                
+                printf("%s 登陆成功\n",masg.send_mem);
+                printf("%s %s %s\n",row[0],row[1],row[2]);
+              ///////注册进在线列表
+              sprintf(sql,"INSERT INTO client_online (socket,name) VALUES('%d','%s')",socket_c,masg.send_mem);
+              if(0 != mysql_real_query(&mysql,sql,strlen(sql))){
+                printf("query: %s\n",mysql_error(&mysql));
+              }
+              printf("%s 加入在线列表\n",masg.send_mem);
+              //////////////////
+
+                masg.flag = LOG_SUC;
+                send(socket_c,&masg,sizeof(MASG),0);
+              }
+              else if ((!mysql_fetch_row(res)))
+              {
+                printf("登录失败！\n");
+                masg.flag = LOG_FAU;
+                send(socket_c,&masg,sizeof(MASG),0);
+              }
+              break;
+
+              case QUIT:
+
+              printf("%s 已下线！\n",masg.send_mem);
+              //从在线表中删除
+              sprintf(sql,"DELETE FROM client_online WHERE name ='%s'",masg.send_mem);
+              printf("从在线用户表中删除..\n");
+              if(0 != mysql_real_query(&mysql,sql,strlen(sql))){
+                printf("query: %s\n",mysql_error(&mysql));
+              }
+              masg.flag = QUIT;
+              send(socket_c,&masg,sizeof(MASG),0);
+              break;
+
+              case ADD_FRI:
+              
+              add_fri(&mysql,&masg,socket_c);
+              break;
+
+              case ADD_FRI_SCU: //添加好友成功
+              //简化版
+              printf("%s 同意了 %d 的好友请求!\n",masg.recv_mem,masg.send_mem);
+              //数据库添加
+              break;
+
+              case ADD_FRI_FAU: //添加好友失败
+              //向发送方发送通知
+              printf("%s 拒绝了 %s 的好友请求!\n",masg.recv_mem,masg.send_mem);
+              break;
+
+            
+
+                      
+
+
+
+            }//switch }
+
+          }//事件 }
+         //  printf("轮寻.......\n");
+        }//for }
+      //  printf("while........\n");
+      } //while }
+    //  printf("服务器关闭.......\n");
+}
